428c08e58e25a9060305cd2de23f8c7edc209dd5
AlisaZil <alisazilberman@gmail.com>
2023-07-26 09:42:26 -0700

    add(main components)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { ToDoItemComponent } from './to-do-item/to-do-item.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent,
+    ToDoItemComponent,
+    TaskItemComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
